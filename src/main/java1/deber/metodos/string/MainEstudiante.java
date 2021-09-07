7e2f9eb7aef01aaf3e363079b12d8d3231f01bf8
Cristian Chulca <cristianchulcam10117@hotmail.com>
2021-09-07 17:41:02 -0500

    Deber corregido.

--- src/main/java1/deber/metodos/string/MainEstudiante.java
+++ src/main/java1/deber/metodos/string/MainEstudiante.java
@@ -3,15 +3,18 @@ package deber.metodos.string;
 import java.util.Scanner;
 
 public class MainEstudiante {
+	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i; 
 
 	Scanner Li = new Scanner(System.in);
 	Scanner Ls = new Scanner(System.in); 
 
 	String c, c1; 
-	String vector[] = new String[4]; 
+	String vector[] = new String[5]; 
 	int op; 
 
 	
@@ -23,24 +26,30 @@ public class MainEstudiante {
 		System.out.println("3.- SALIR.");
 	   op = Li.nextInt(); 
 	   
-   int i = 0; 
 	   if (op == 1) { 
-		    for( i = 0; i <= 4; i++) {
+		   for( i = 0; i <= 4; i++) {
 			System.out.println("Ingresar la cédula del Estudiante: ");
 		    c = Ls.nextLine(); 
+		    
 			vector[i] = c; 
-		    }
-		    }else if(op == 2) {
+		 
+		   }
+		   } else  if(op == 2) {
 				System.out.println("Ingrese cedula del Estudiante a buscar: ");
-                c1 = Ls.nextLine();  
-			   boolean resultado = vector[i].equals(c1); 
-				System.out.println("El estudiante se encuentra en el Vector?"+ resultado);	    
-	   }
+                c1 = Ls.nextLine();      
+			boolean resultado = vector[0].equals(c1); 
+			resultado = vector[1].equals(c1);
+			resultado = vector[2].equals(c1);
+			resultado = vector[3].equals(c1);
+			resultado = vector[4].equals(c1);
+			
+		     		System.out.println("El estudiante se encuentra en el Vector?"+ resultado);	    
+		     }  
 	   
    }while(op != 3);
 	System.out.println("SE SALIO DEL SISTEMA.");
-
+}
 	
 	}
 
-}
+
