76cf2e6de4ad167304fd8e4926bbc6bbace6c912
Cristian Chulca <cristianchulcam10117@hotmail.com>
2021-09-07 20:06:52 -0500

    OHAL

--- src/main/java1/ejercicios/clase/String/Estudiante.java
+++ src/main/java1/ejercicios/clase/String/Estudiante.java
@@ -2,9 +2,39 @@ package ejercicios.clase.String;
 
 public class Estudiante {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+private String nombre; 
+private String apellido; 
+public static String ciudad = "Machachi"; 
+public static String parroquia = "Aloag;";
 
-	}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getApellido() {
+	return apellido;
+}
+public void setApellido(String apellido) {
+	this.apellido = apellido;
+}
+public static String getCiudad() {
+	return ciudad;
+}
+public static void setCiudad(String ciudad) {
+	Estudiante.ciudad = ciudad;
+}
+public static String getParroquia() {
+	return parroquia;
+}
+public static void setParroquia(String parroquia) {
+	Estudiante.parroquia = parroquia;
+}
 
+
+
+	
+	
+	
 }
