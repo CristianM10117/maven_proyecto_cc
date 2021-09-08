876eb7b9140e5369d62c71f80fd9f8c7638a829d
Cristian Chulca <cristianchulcam10117@hotmail.com>
2021-09-07 20:05:24 -0500

    HOLA

---
+++ src/main/java1/ejercicios/clase/String/MainEstudiante.java
@@ -0,0 +1,13 @@
+package ejercicios.clase.String;
+
+public class MainEstudiante {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("Valor del atributo estatico: " +Estudiante.ciudad);
+	System.out.println("Valor del atributo estatico: " +Estudiante.parroquia);
+	
+	}
+
+}
