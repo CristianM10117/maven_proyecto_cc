85c36de283f0ef01b452247918575fa60f49e401
Cristian Chulca <cristianchulcam10117@hotmail.com>
2021-09-17 20:08:56 -0500

    Ejercicio list.

---
+++ src/main/java1/ejercicios/coleccion/list/Main.java
@@ -0,0 +1,24 @@
+package ejercicios.coleccion.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	Integer numero1 = 1; 
+	Integer numero2 = 4;
+	Integer numero3 = 8;
+	Integer numero4 = 20;
+	
+		List<Integer> listaNueva = new ArrayList<Integer>(); 
+	listaNueva.add(numero1);
+	listaNueva.add(numero2);
+	listaNueva.add(numero3);
+	listaNueva.add(numero4);
+	
+	int vector[] = new int [10];
+	}
+}
